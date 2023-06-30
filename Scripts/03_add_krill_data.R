470e173ec36146a7937f23238dac6b1b90882e13
Lorenzo Davidson <lorenzodavidson@Lorenzos-MacBook-Pro.local>
2023-06-30 16:10:11 -0700

    Stats for pres abs

--- Scripts/03_add_krill_data.R
+++ Scripts/03_add_krill_data.R
@@ -107,11 +107,68 @@ extract_fn <- function(df) {
 # Extracting for all 40 presabs datasets -----------------------------------
 
 blwh_ROMS_krill <- blwh_ROMS
-row_num <- matrix(NA,1,40)
+blwh_ROMS_krillchl_filtered <- list() # Only data points with krill values
 for (i in 1:40) {
   blwh_ROMS_krill[[i]] <- extract_fn(blwh_ROMS[[i]])
-  row_num[i] <-   nrow(blwh_ROMS_krill[[i]][complete.cases(blwh_ROMS_krill[[i]]$krill),])
+  blwh_ROMS_krillchl_filtered[[i]] <- blwh_ROMS_krill[[i]][rowSums(is.na(blwh_ROMS_krill[[i]][c(1,54)])) == 0,][c(1:2,5:8,50,54)]
 }
 
-hist(row_num,xlab="Number of data points with krill")
-abline(v = mean(row_num),col = "red",lwd = 3)
+# General statistics on krill data points ----------------------------------
+
+row_num <- matrix(NA,1,40)
+row_num_krill <- matrix(NA,1,40)
+row_num_spatial <- matrix(NA,1,40) # Data points Apr-Aug including those without krill
+for (i in 1:40) {
+  row_num[i] <- nrow(blwh_ROMS_krill[[i]])
+  row_num_krill[i] <-   nrow(blwh_ROMS_krillchl_filtered[[i]])
+  row_num_spatial[i] <- nrow(filter(blwh_ROMS_krill[[i]],month==4|month==5|month==6|month==7|month==8))
+}
+
+# Blwh data points with krill (all 40 presabs dataset)
+avg_total_points <- round(mean(row_num),0)
+
+hist(row_num_krill,xlab="Data points",main="Total Number of blwh data points with krill")
+abline(v = mean(row_num_krill),col = "red",lwd = 3)
+text(x=3325,y=12,labels=paste0("Out of total ",avg_total_points," points"),col = "red")
+
+# Fraction of blwh data points in Apr-Aug that have krill (all 40 presabs dataset)
+fraction_num_spatial <- row_num_krill/row_num_spatial
+percent_missed_spatial <- round(100*(1-mean(fraction_num_spatial)),2)
+
+hist(fraction_num_spatial,xlab="Data points",main="Fraction of Apr-Aug blwh data points that have krill")
+abline(v = mean(fraction_num_spatial),col = "red",lwd = 3)
+text(x=0.715,y=8,labels=paste0("Avg points missed = ",percent_missed_spatial,"%"),col = "red")
+
+# Fraction of blwh data points that have krill (all 40 presabs dataset)
+fraction_num_temporal <- row_num_krill/row_num
+percent_missed_temporal <- round(100*(1-mean(fraction_num_temporal)),2)
+
+hist(fraction_num_temporal,xlab="Data points",main="Fraction of blwh data points that have krill")
+abline(v = mean(fraction_num_temporal),col = "red",lwd = 3)
+text(x=0.145,y=8,labels=paste0("Avg points missed = ",percent_missed_temporal,"%"),col = "red")
+
+
+# Comparing pres and abs krill and chl statistics -------------------------
+
+blwh_ROMS_1 <- blwh_ROMS_krill[[1]]
+blwh_krillchl_1 <- blwh_ROMS_1[complete.cases(blwh_ROMS_krill[[i]]$krill),][c(1:2,5:8,50,54)]
+blwh_pres_1 <- filter(blwh_krillchl_1,presabs==1)
+blwh_abs_1 <- filter(blwh_krillchl_1,presabs!=1)
+blwh_pres_num <- blwh_pres_1[complete.cases(blwh_pres_1$krill),]
+blwh_abs_num <- blwh_abs_1[complete.cases(blwh_abs_1$krill),]
+
+# Krill
+ggplot() +
+  geom_area(data=blwh_pres_1, aes(x=krill), stat = "bin",binwidth=.2,
+            color= "blue",fill="lightblue",alpha=0.7) +
+  geom_area(data=blwh_abs_1, aes(x=krill), stat = "bin",binwidth=.2,
+            color= "red",fill="pink",alpha=0.7) +
+  legend
+  
+  
+
+
+  
+
+# Plot
+layer(sp.points(xy, pch=ifelse(xy$z1 < 0.5, 2, 3), cex=2, col=1), columns=1)
